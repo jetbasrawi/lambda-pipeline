5ca9898c0f0ee5c96c5a0b4b3ebb4c6f8e849c8a
jetbasrawi <anewexplorer@gmail.com>
2017-12-23 14:05:01 +0000

    made a small change to trigger a deployment

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ exports.handler = (event, context, callback) => {
 	currentTime.setTimeZone("America/Los_Angeles");
 	callback(null, {
 			statusCode: '200',
-			body: 'The time in los Angeles is: ' + currentTime.toString(),
+			body: 'The time right now in los Angeles is: ' + currentTime.toString(),
 	});
 }
